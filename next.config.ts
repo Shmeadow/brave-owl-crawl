3a6e23ccafb0578fa8366120e3667d9d17cdf601
[dyad] <xdeekro@gmail.com>
2025-07-02 12:33:17 +0300

    [dyad] wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,16 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mrdupsekghsnbooyrdmj.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
