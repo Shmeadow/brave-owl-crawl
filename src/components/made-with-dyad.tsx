b259a6d7282d08d6788f12ed3585f571c8d152fd
[dyad] <xdeekro@gmail.com>
2025-07-16 23:18:10 +0300

    [dyad] Removed unused admin settings and blog pages, and an unused component to streamline the codebase. - deleted 4 file(s)

--- src/components/made-with-dyad.tsx
+++
@@ -1,7 +0,0 @@
-export const MadeWithDyad = () => {
-  return (
-    <div className="p-4 text-center bg-background rounded-lg mt-8 backdrop-blur-xl"> {/* Added backdrop-blur-xl */}
-      {/* Content removed as per request */}
-    </div>
-  );
-};
