03f8e57feabd230908c88df5f03dac114c9ebf50
[dyad] <xdeekro@gmail.com>
2025-07-17 11:40:21 +0300

    Reverted all changes back to version 023619edae16a714a092130263e8e58d0284db68

--- src/components/widget-content/drawing-board-widget.tsx
+++ src/components/widget-content/drawing-board-widget.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import React from 'react';
-import { Excalidraw } from '@excalidraw/excalidraw';
-import { useTheme } from 'next-themes';
-import "@excalidraw/excalidraw/index.css"; // Import Excalidraw styles
 
 interface DrawingBoardWidgetProps {
   isCurrentRoomWritable: boolean;
@@ -11,20 +8,14 @@ interface DrawingBoardWidgetProps {
 }
 
 export function DrawingBoardWidget({ isCurrentRoomWritable, isMobile }: DrawingBoardWidgetProps) {
-  const { theme } = useTheme();
-
   return (
     <div className="h-full w-full">
-      <Excalidraw
-        theme={theme === 'dark' || theme === 'cozy' ? 'dark' : 'light'}
-        UIOptions={{
-          canvasActions: {
-            loadScene: false, // Disabling file loading for simplicity
-            saveToActiveFile: false,
-            saveAsImage: true,
-          },
-        }}
-      />
+      <iframe
+        src="https://sketch.io/sketchpad/"
+        className="w-full h-full border-0"
+        title="Sketch.io Drawing Board"
+        allow="fullscreen"
+      ></iframe>
     </div>
   );
 }
