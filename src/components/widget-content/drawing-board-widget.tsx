e5f12c9d48720fde62eea745779a421d0feb93bc
[dyad] <xdeekro@gmail.com>
2025-07-17 21:04:05 +0300

    [dyad] wrote 4 file(s), added @tldraw/tldraw package(s)

--- src/components/widget-content/drawing-board-widget.tsx
+++ src/components/widget-content/drawing-board-widget.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React from 'react';
+import { Tldraw } from '@tldraw/tldraw';
+import '@tldraw/tldraw/tldraw.css'; // Import tldraw styles
 
 interface DrawingBoardWidgetProps {
   isCurrentRoomWritable: boolean;
@@ -10,12 +12,7 @@ interface DrawingBoardWidgetProps {
 export function DrawingBoardWidget({ isCurrentRoomWritable, isMobile }: DrawingBoardWidgetProps) {
   return (
     <div className="h-full w-full">
-      <iframe
-        src="https://sketch.io/sketchpad/"
-        className="w-full h-full border-0"
-        title="Sketch.io Drawing Board"
-        allow="fullscreen"
-      ></iframe>
+      <Tldraw />
     </div>
   );
 }
