d0028c10184dca5e012051142f225777b289b84b
[dyad] <xdeekro@gmail.com>
2025-07-17 11:39:23 +0300

    [dyad] Replacing the drawing board with Excalidraw - wrote 3 file(s), added @excalidraw/excalidraw package(s)

--- src/components/widget-content/drawing-board-widget.tsx
+++ src/components/widget-content/drawing-board-widget.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import React from 'react';
+import { Excalidraw } from '@excalidraw/excalidraw';
+import { useTheme } from 'next-themes';
+import "@excalidraw/excalidraw/index.css"; // Import Excalidraw styles
 
 interface DrawingBoardWidgetProps {
   isCurrentRoomWritable: boolean;
@@ -8,14 +11,20 @@ interface DrawingBoardWidgetProps {
 }
 
 export function DrawingBoardWidget({ isCurrentRoomWritable, isMobile }: DrawingBoardWidgetProps) {
+  const { theme } = useTheme();
+
   return (
     <div className="h-full w-full">
-      <iframe
-        src="https://sketch.io/sketchpad/"
-        className="w-full h-full border-0"
-        title="Sketch.io Drawing Board"
-        allow="fullscreen"
-      ></iframe>
+      <Excalidraw
+        theme={theme === 'dark' || theme === 'cozy' ? 'dark' : 'light'}
+        UIOptions={{
+          canvasActions: {
+            loadScene: false, // Disabling file loading for simplicity
+            saveToActiveFile: false,
+            saveAsImage: true,
+          },
+        }}
+      />
     </div>
   );
 }
